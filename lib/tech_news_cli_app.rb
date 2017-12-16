67499c3cebe928cd235defbd6cd9f9e16b6644db
Richard Chang <rchang9999@yahoo.com>
2017-12-16 22:21:41 +0000

    trying to fix filepaths error

--- lib/tech_news_cli_app.rb
+++ lib/tech_news_cli_app.rb
@@ -1,11 +1,3 @@
-require "nokogiri"
-require "open-uri"
-require "pry"
-
-
 require_relative "tech_news_cli_app/cli"
 require_relative "tech_news_cli_app/news"
 require_relative "tech_news_cli_app/version"
-
-module TechNews
-end
--- lib/tech_news_cli_app/cli.rb
+++ lib/tech_news_cli_app/cli.rb
@@ -1,4 +1,1 @@
-class TechNews::CLI
-
-
-end
+puts "hello"
