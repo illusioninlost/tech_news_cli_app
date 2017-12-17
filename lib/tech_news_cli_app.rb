a686b5d7e90e6dd83b786e30967e19ca0953406b
Richard Chang <rchang9999@yahoo.com>
2017-12-17 03:15:52 +0000

    changed files, still uninitialize constant error

--- lib/tech_news_cli_app.rb
+++ lib/tech_news_cli_app.rb
@@ -1,3 +1,6 @@
 require_relative "tech_news_cli_app/cli"
 require_relative "tech_news_cli_app/news"
 require_relative "tech_news_cli_app/version"
+
+module TechNews
+end
--- lib/tech_news_cli_app/news.rb
+++ lib/tech_news_cli_app/news.rb
@@ -1,1 +1,2 @@
-puts "hello"
+class TechNews::news
+end
